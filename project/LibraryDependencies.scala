6ef00a49e798776dbaf13a3f50f65093af65d5f1
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2023-12-22 09:56:34 +0000

    APIS-6633 - Evergreening

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -4,7 +4,7 @@ import play.sbt.PlayImport._
 object LibraryDependencies {
   def apply() = compileDependencies ++ testDependencies
 
-  lazy val bootstrapVersion = "7.12.0"
+  lazy val bootstrapVersion = "7.15.0"
 
   lazy val compileDependencies = Seq(
     "com.typesafe.play"       %% "play-json"                        % "2.9.2",
@@ -12,8 +12,9 @@ object LibraryDependencies {
   )
 
   lazy val testDependencies = Seq(
-    "org.scalatest"           %% "scalatest"                        % "3.2.9",
-    "com.github.tomakehurst"  %  "wiremock-jre8-standalone"         % "2.27.2",
-    "com.vladsch.flexmark"    %  "flexmark-all"                     % "0.35.10"       
+    "com.vladsch.flexmark"    %  "flexmark-all"                     % "0.62.2",
+    "com.github.tomakehurst"  %  "wiremock-jre8-standalone"         % "2.31.0",
+    "org.mockito"             %% "mockito-scala-scalatest"          % "1.17.29",
+    "org.scalatest"           %% "scalatest"                        % "3.2.17"
   ).map(_ % Test)
 }
