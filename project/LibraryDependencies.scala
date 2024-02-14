bcaf32699989cef71dd9f53615291ce73e1668ed
Toby Gilham <623674+Tobias1087@users.noreply.github.com>
2024-02-14 07:52:42 +0000

    APIS-6813 - Play 3.0 upgrade

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -7,14 +7,14 @@ object LibraryDependencies {
   lazy val bootstrapVersion = "7.15.0"
 
   lazy val compileDependencies = Seq(
-    "com.typesafe.play"       %% "play-json"                        % "2.9.2",
-    "org.raml"                %  "raml-parser-2"                    % "1.0.13"
+    "org.playframework" %% "play-json"     % "3.0.1",
+    "org.raml"           % "raml-parser-2" % "1.0.13"
   )
 
   lazy val testDependencies = Seq(
-    "com.vladsch.flexmark"    %  "flexmark-all"                     % "0.62.2",
-    "com.github.tomakehurst"  %  "wiremock-jre8-standalone"         % "2.31.0",
-    "org.mockito"             %% "mockito-scala-scalatest"          % "1.17.29",
-    "org.scalatest"           %% "scalatest"                        % "3.2.17"
+    "com.vladsch.flexmark"    % "flexmark-all"            % "0.64.6",
+    "org.mockito"            %% "mockito-scala-scalatest" % "1.17.29",
+    "com.github.tomakehurst"  % "wiremock"                % "3.0.0-beta-7",
+    "org.scalatest"          %% "scalatest"               % "3.2.17"
   ).map(_ % Test)
 }
